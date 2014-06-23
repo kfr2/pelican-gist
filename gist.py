2e8c277a5d4054f1fd94a8806678d58d0994374c
Kevin Richardson <kevin@magically.us>
2014-06-23 18:43:17 -0400

    Accept any word character in gist ID
    Gist IDs were previously numeric; they can now be hex strings.

--- gist.py
+++ gist.py
@@ -5,7 +5,7 @@ from pelican import signals
 
 def gist(content):
     """Converts [[ gist username:ID ]] in an article into its embedded <script> equivalent."""
-    pattern = r'\[\[ gist (\w+):(\d+) \]\]'
+    pattern = r'\[\[ gist (\w+):(\w+) \]\]'
     replacement = r'<script src="https://gist.github.com/\1/\2.js"></script>'
     content._content = re.sub(pattern, replacement, unicode(content._content))
 
